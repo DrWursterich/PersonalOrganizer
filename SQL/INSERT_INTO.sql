b38e40f35acce4ce8a265389f50048455e57ca86
DrWursterich <31037782+DrWursterich@users.noreply.github.com>
2018-07-22 15:13:38 +0200

    Updated SQL-Scripts
    Changed Datatype DATE to INTEGER,
    Added and changed fixed PERIODs,
    Refactoring

--- SQL/INSERT_INTO.sql
+++ SQL/INSERT_INTO.sql
@@ -6,23 +6,25 @@
 	(3,		'LOW');
 
 --PERIOD
- INSERT OR IGNORE INTO PERIOD(ID, MINUTES, DAYS, MONTHS) VALUES
-	(1, 0, 1, 0),
-	(2, 180, 0, 0);
+ INSERT OR IGNORE INTO PERIOD
+	(ID,	MINUTES,	DAYS,	MONTHS) VALUES
+	(1,		0,			1,		0),
+	(2,		180,		0,		0);
 
 --PERIOD
  INSERT OR IGNORE INTO PERIOD
 	(ID,	MINUTES,	DAYS,	MONTHS) VALUES
+	(1,		0,			0,		0),
+	(2,		24,			0,		0),
 	(3,		60,			0,		0),
 	(4,		15,			0,		0);
 
 --PRIORITY_PERIOD
  INSERT OR IGNORE INTO PRIORITY_PERIOD
 	(ID,	PRIORITY_FK,	PERIOD_FK) VALUES
-	(1,		1,				1),
-	(2,		1,				2),
-	(3,		1,				3),
-	(4,		1,				4),
+	(1,		1,				2),
+	(2,		1,				3),
+	(3,		1,				4),
 	(5,		2,				3),
 	(6,		2,				4);
 
