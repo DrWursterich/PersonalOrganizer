1595fe197ee1a14db0d9b0eec89c987c016d9aa3
DrWursterich <31037782+DrWursterich@users.noreply.github.com>
2018-07-22 15:19:07 +0200

    Added arithmetic Functions for Duration and GregorianCalender Objects

--- src/util/Duration.java
+++ src/util/Duration.java
@@ -2,9 +2,15 @@ package util;
 
 import java.util.GregorianCalendar;
 
-public class Duration {
-	private short months;
-	private long minutes;
+/**
+ * Immutable class to represent a timeperiod defined by months and minutes.
+ * @author Mario Schäper
+ */
+public class Duration implements Comparable<Duration> {
+	private final static int MINUTES_PER_MONTH = 27 * 24 * 60;
+
+	private final short months;
+	private final long minutes;
 
 	public Duration(int months, int days, long hours, long minutes) {
 		this.months = (short)months;
@@ -18,26 +24,149 @@ public class Duration {
 		this.minutes = (to.getTimeInMillis() - tmp.getTimeInMillis()) / 60000;
 	}
 
+	public Duration(GregorianCalendar from, GregorianCalendar to) {
+		this(from, to, false);
+	}
+
+	/**
+	 * Returns the amount of months.
+	 * @return months
+	 */
 	public int getMonths() {
 		return this.months;
 	}
 
+	/**
+	 * Returns the amount of days.
+	 * @return days
+	 */
 	public int getDays() {
 		return (int)(this.minutes / 60 / 24);
 	}
 
+	/**
+	 * Returns the amount of hours.
+	 * @return hours
+	 */
 	public int getHours() {
 		return (int)(this.minutes / 60 % 24);
 	}
 
+	/**
+	 * Returns the amount of minutes.
+	 * @return minuts
+	 */
 	public int getMinutes() {
 		return (int)(this.minutes % 60);
 	}
 
+	/**
+	 * Returns the minutes, hours and days in minutes.
+	 * @return minutes
+	 */
 	public long getRawMinutes() {
 		return this.minutes;
 	}
 
+	/**
+	 * Returns a {@link java.util.GregorianCalendar GregorianCalendar} instance
+	 * with the summed values of both parameters.
+	 * @param augend the augend
+	 * @param addend the addend
+	 * @return resulting {@link java.util.GregorianCalendar GregorianCalendar} instance
+	 */
+	public static GregorianCalendar add(GregorianCalendar augend, Duration addend) {
+		GregorianCalendar ret = (GregorianCalendar)augend.clone();
+		ret.add(GregorianCalendar.MONTH, addend.months);
+		ret.add(GregorianCalendar.DAY_OF_MONTH, addend.getDays());
+		ret.add(GregorianCalendar.HOUR_OF_DAY, addend.getHours());
+		ret.add(GregorianCalendar.MINUTE, addend.getMinutes());
+		return ret;
+	}
+
+	/**
+	 * Returns a {@link Duration Duration} instance with the summed values of both parameters.
+	 * @param augend the augend
+	 * @param addend the addend
+	 * @return resulting {@link Duration Duration} instance
+	 */
+	public static Duration add(Duration augend, Duration addend) {
+		return new Duration(augend.months + addend.months, 0, 0, augend.minutes + addend.minutes);
+	}
+
+	/**
+	 * Returns a {@link java.util.GregorianCalendar GregorianCalendar} instance
+	 * with the subtracted values of the subtrahend from the minuend.
+	 * @param minuend the minuend
+	 * @param subtrahend the subtrahend
+	 * @return resulting {@link java.util.GregorianCalendar GregorianCalendar} instance
+	 */
+	public static GregorianCalendar subtract(GregorianCalendar minuend, Duration subtrahend) {
+		GregorianCalendar ret = (GregorianCalendar)minuend.clone();
+		ret.add(GregorianCalendar.MINUTE, subtrahend.getMinutes() * -1);
+		ret.add(GregorianCalendar.HOUR_OF_DAY, subtrahend.getHours() * -1);
+		ret.add(GregorianCalendar.DAY_OF_MONTH, subtrahend.getDays() * -1);
+		ret.add(GregorianCalendar.MONTH, subtrahend.months * -1);
+		return ret;
+	}
+
+	/**
+	 * Returns a {@link Duration Duration} instance with the subtracted values
+	 * form the subtrahend of the minuend.
+	 * @param minuend the minuend
+	 * @param subtrahend the subtrahend
+	 * @return resulting {@link Duration Duration} instance
+	 */
+	public static Duration subtract(Duration minuend, Duration subtrahend) {
+		return new Duration(minuend.months - subtrahend.months, 0, 0,
+				minuend.minutes - subtrahend.minutes);
+	}
+
+	/**
+	 * Returns a {@link Duration Duration} instance with the values from the multiplicand
+	 * multiplied by the multiplier.
+	 * @param multiplicand the multiplicand
+	 * @param multiplier the multiplier
+	 * @returnresulting {@link Duration Duration} instance
+	 */
+	public static Duration multiply(Duration multiplicand, int multiplier) {
+		return new Duration(multiplicand.months * multiplier, 0, 0,
+				multiplicand.minutes * multiplier);
+	}
+
+	/**
+	 * Returns a {@link Duration Duration} instance with the values of the quotient of the
+	 * dividend and divisor. Months and minutes are divided seperatly.
+	 * Floatingpoint months are replaced by the minimum of days of a month as
+	 * minutes. Resulting values are rounded down.
+	 * @param dividend the dividend
+	 * @param divisor the divisor
+	 * @return resulting {@link Duration Duration} instance
+	 */
+	public static Duration divide(Duration dividend, int divisor) {
+		return new Duration(dividend.months / divisor, 0, 0, dividend.minutes / divisor
+				+ (dividend.months % divisor) * MINUTES_PER_MONTH / divisor);
+	}
+
+	/**
+	 * Returns the quotient of the dividend and devisor.
+	 * Months are replaced by the minimum of days of a month as minutes.
+	 * @param dividend the dividend
+	 * @param divisor the divisor
+	 * @return the quotient
+	 */
+	public static double divide(Duration dividend, Duration divisor) {
+		return (dividend.months * MINUTES_PER_MONTH + dividend.minutes) /
+				(divisor.months * MINUTES_PER_MONTH + divisor.minutes);
+	}
+
+	/**
+	 * Returns the amount of months between two
+	 * {@link java.util.GregorianCalendar GregorianCalendar} instances.
+	 * @param from first date
+	 * @param to second date
+	 * @return months
+	 */
 	private int monthsBetween(GregorianCalendar from, GregorianCalendar to) {
 		int months = 0;
 		GregorianCalendar tmp = (GregorianCalendar)from.clone();
@@ -47,5 +176,31 @@ public class Duration {
 			tmp.add(GregorianCalendar.MONTH, 1);
 		}
 		return months;
+	}
+
+	@Override
+	public String toString() {
+		return this.getHours() + ":" + this.getMinutes() + " "
+				+ this.getDays() + "." + this.getMonths() + ".";
+	}
+
+	@Override
+	public int hashCode() {
+		return Integer.hashCode(this.months) + Long.hashCode(this.minutes);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof Duration)) {
+			return false;
+		}
+		return this.months == ((Duration)other).months && this.minutes == ((Duration)other).minutes;
+	}
+
+	@Override
+	public int compareTo(Duration other) {
+		return Long.compare(
+				this.months * MINUTES_PER_MONTH + this.minutes,
+				other.months * MINUTES_PER_MONTH + other.minutes);
 	}
 }
